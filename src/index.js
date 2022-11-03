b44ef10b968cc402d3f51d50230d921df9fc73a0
gunwoongPark <bung1438@gmail.com>
2022-11-04 05:03:16 +0900

    feat: 기본 폼 세팅

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 const $ = document.querySelector.bind(document);
 
+const renderInputTodoView = () => {
+  return `
+    <span class="input-container">
+      <input class="todo-input"/>
+      <button class="save-button">등록</button>
+    </span> 
+  `;
+};
+
+const renderTodoListView = (todoList) => {
+  return `
+    <ul class="todo-container">
+      ${todoList.reduce(
+        (html, todo) => `${html} <li class="todo">${todo.todo}</li>`,
+        ``
+      )}
+    </ul>
+  `;
+};
+
+const render = (todoList) => {
+  const inputTodoView = renderInputTodoView();
+  const todoListView = renderTodoListView(todoList);
+
+  return `
+    <div class="wrapper">
+      <h1>TODO APP</h1>
+      ${inputTodoView}
+      ${todoListView}
+    </div>
+  `;
+};
+
 (() => {
-  $("#app").innerHTML = "HELLO TODO WORLD";
+  const todoList = [
+    { id: 1, todo: "먹기" },
+    { id: 2, todo: "자기" },
+    { id: 3, todo: "싸기" },
+  ];
+
+  $("#app").innerHTML = render(todoList);
 })();
